5eaa6236c55b836547b03b4d03d193da1df54268
binduvelivela <bindu.velivela0202@gmail.com>
2022-12-12 21:44:05 -0500

    Add

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy;
-const routes = require('./routes.js');
-require('dotenv').config();
-app.use(express.static('public'));
+const session = require("express-session");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const routes = require("./routes.js");
+require("dotenv").config();
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'SECRET'
-}));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET",
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,20 +28,23 @@ passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
-passport.use(new FacebookStrategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.SECRET,
-  callbackURL: process.env.CALLBACK_URL
-}, function (accessToken, refreshToken, profile, done) {
-  return done(null, profile);
-}
-));
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
 
-app.use('/', routes);
-
+app.use("/", routes);
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log('App listening on port ' + port);
+  console.log("App listening on port " + port);
 });
